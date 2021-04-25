fcd6017a175c22e1a213699c7566c3cb13a60410
Carlos Guzman <cags84@gmail.com>
2021-04-25 11:47:36 -0500

    Add .vscode folder, modifify .eslintrc
    Add .vscode folder with three files for correct editor configuration.
    Modify .eslintrc file, for correct integration of prettier and eslint.
    
    No forget restart the editor vscode.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 module.exports = {
   env: {
-    browser: true,
     commonjs: true,
     es2021: true,
+    node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
+    'no-console': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: 'next',
+      },
+    ],
   },
 };
