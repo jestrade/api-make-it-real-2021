743ef0b22cf4ce0a924936fc4e4179762ec044e4
Jesus Estrada <jesuestrada@gmail.com>
2021-04-29 19:57:52 -0500

    authentication login

--- http/index.js
+++ http/index.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const api = require('../api');
-const { config } = require('../config');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const api = require("../api");
+const { config } = require("../config");
 
 const { host, port } = config.http;
 
 const app = express();
 app.use(express.json());
-app.use('/api', api);
-app.use('/api/v1', api);
-app.use(express.static('public'));
+app.use(cookieParser());
+app.use("/api", api);
+app.use("/api/v1", api);
+app.use(express.static("public"));
 
 const init = () => {
   app.listen(port, host, () => {
