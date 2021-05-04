fb59440777e3d3ecb961c38a8ba78d4c4a173a22
Carlos Guzmán <cags84@gmail.com>
2021-05-04 13:50:14 +0000

    Modifify - files `locale/en-US.js`, `locale/es-CO.js` where modified
    - the necessary data was added

--- locale/en-US.js
+++ locale/en-US.js
@@ -1,12 +1,28 @@
 const dictionaryENUS = {
   errors: {
-    notAuthorized: 'unauthorized',
-    notAuthenticated: 'user not authorized',
-    invalidData: 'invalid data',
+    notAuthorized: "unauthorized",
+    notAuthenticated: "user not authorized",
+    invalidData: "invalid data",
     user: {
-      userExists: 'user already exists',
-      userNotExists: 'user not exists',
-      onCreate: 'error while creating user',
+      userExists: "user already exists",
+      userNotExists: "user not exists",
+      onCreate: "error while creating user",
+      onDelete: "error while deleted user",
+    },
+    tweet: {
+      onDelete: "error while deleted tweet",
+    },
+  },
+  success: {
+    user: {
+      onCreate: "user created successfully",
+      onUpdate: "user updated successfully",
+      userDeleted: "user removed successfully",
+    },
+    tweet: {
+      onCreate: "tweet created successfully",
+      onUpdate: "tweet updated successfully",
+      tweetDeleted: "tweet removed successfully",
     },
   },
 };
--- locale/es-CO.js
+++ locale/es-CO.js
@@ -1,8 +1,29 @@
 const dictionaryESCO = {
   errors: {
-    notAuthorized: 'no autorizado',
-    notAuthenticated: 'no autenticado',
-    invalidData: 'datos no válidos',
+    notAuthorized: "no autorizado",
+    notAuthenticated: "no autenticado",
+    invalidData: "datos no válidos",
+    user: {
+      userExists: "usuario actualmente existe",
+      userNotExists: "usuario no existe",
+      onCreate: "error al intentar crear el usuario",
+      onDelete: "error al intentar eliminar el usuario",
+    },
+    tweet: {
+      onDelete: "error al intentar eliminar el tweet",
+    },
+  },
+  success: {
+    user: {
+      onCreate: "usuario creado exitosamente",
+      onUpdate: "usuario actualizado correctamente",
+      userDeleted: "usuario eliminado correctamente",
+    },
+    tweet: {
+      onCreate: "tweet creado correctamente",
+      onUpdate: "tweet actualizado correctamente",
+      tweetDeleted: "tweet eliminado correctamente",
+    },
   },
 };
 
