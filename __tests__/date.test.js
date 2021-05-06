1a2fed82ce2f451a711ba8da79eefcc8a0a3aa7b
Jesus Estrada <jesuestrada@gmail.com>
2021-05-06 00:43:36 -0500

    date

--- __tests__/date.test.js
+++ __tests__/date.test.js
@@ -2,8 +2,9 @@ const { formatDate } = require("./../libs/date");
 
 describe("format date", () => {
   it("is ok: date, time", () => {
-    const date = "2021-05-06T04:36:33.137Z";
-    expect(formatDate(date)).toBe("5/5/2021, 11:36:33 PM");
+    const date = "Sat Jan 01 2000 00:00:00 GMT-0500";
+    const expectedDate = new Date(date).toLocaleString();
+    expect(formatDate(date)).toBe(expectedDate);
   });
   it("is undefined", () => {
     let date;
